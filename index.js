5225c43d2e77e08f9bfded5f5fb2e591648611a1
traveldevel <31585321+traveldevel@users.noreply.github.com>
2017-09-14 00:55:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,61 +5,70 @@ var MongoClient = require('mongodb').MongoClient;
 var cors = require("cors");
 var cfenv = require("cfenv");
 
-// configs from env vars
-var appEnv = cfenv.getAppEnv();
-
-// Create app variable to initialize Express 
-var app = express();
-
-// Enable Cross-origin resource sharing (CORS)  for app.
-app.use(cors());
-
-// Define Odata model of the resource entity i.e. Product. 
-// The metadata is defined using OData type system called the Entity Data Model (EDM),
-// consisting of EntitySets, Entities, ComplexTypes and Scalar Types.
-var model = {
-    namespace: "iothub",
-    entityTypes: {
-        "user": {
-            "_id": {"type": "Edm.String", key: true},        
-            "name": {"type": "Edm.String"},
-            "password": {"type": "Edm.String"},  
-            "roles": {"type": "Edm.String"}                
-        }
-    },   
-    entitySets: {
-        "users": {
-            entityType: "iothub.user"
-        }
-    }
-};
-
-// Instantiates ODataServer and assigns to odataserver variable.
-var odataServer = ODataServer().model(model);
-
 var landscapeName = process.env.landscapeName;
 var tenantName = process.env.tenantName;
 
+// Create app variable to initialize Express 
+var app = express();
+app.use(cors());
+
 // mongo connect and create missing collections
+var appEnv = cfenv.getAppEnv();
 var mongoServiceName = "iot_hub_mongo_" + landscapeName;
 var mongoService = appEnv.getService(mongoServiceName);
 var mongoCredentials = appEnv.getServiceCreds(mongoServiceName);
 var mongoUrl = mongoCredentials.uri;
 var mongoClient = require('mongodb').MongoClient;
 
-console.log(mongoServiceName + " found in VCAP_SERVICES : ")
-//console.log(mongoService);
+console.log(mongoServiceName + " found in VCAP_SERVICES");
+console.log(mongoService.credentials);
 
-var mongoCredentials = {};
+var mongoDbName = '';
 var mongoUrl = '';
 
 if(mongoService !== undefined){
-    mongoCredentials = mongoService.credentials;
-    mongoUrl = mongoCredentials.uri;
+
+    mongoUrl = mongoService.credentials.uri + "?ssl=false";
+
+    var mongodbUri = require('mongodb-uri');
+    var uriObject = mongodbUri.parse(mongoUrl);
+    mongoDbName = uriObject.database;
 }
 
-// Connection to demo database in MongoDB
+console.log("Mongo url : ", mongoUrl);
+console.log("Mongo db : ", mongoDbName);
+
+var model = {
+    namespace: mongoDbName,
+    entityTypes: {
+        'user': {
+            "_id": { "type": "Edm.String", key: true},        
+            "name": { "type": "Edm.String"},
+            "password": { "type": "Edm.String"},  
+            "roles": { "type": "Collection(Edm.String)"}                
+        }
+    },   
+    entitySets: {}
+};
+
+model.entitySets[tenantName + "_user"] = { entityType: mongoDbName + ".user" };
+
+// Instantiates ODataServer and assigns to odataserver variable.
+var odataServer = ODataServer().model(model);
+odataServer.cors('*');
+
+odataServer.error(function(req, res, error, next){
+    console.log(err);
+    next();
+})
+
+// Connection to database in MongoDB
 MongoClient.connect(mongoUrl, function(err, db) {
+    
+    if(err){
+        console.log(err);
+    }
+
     odataServer.onMongo(function(cb) { cb(err, db); });
 });
 
@@ -68,7 +77,7 @@ app.use("/", function (req, res) {
     odataServer.handle(req, res);
 });
 
-// The app listens on port 3010 and prints the endpoint URI in console window.
+// The app listens on port 8080 and prints the endpoint URI in console window.
 var server = app.listen(8080, function () {
     console.log('OData service listening on ' + appEnv.url + ':' + process.env.PORT);
 });
