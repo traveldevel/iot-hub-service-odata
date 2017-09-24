9c85b6024b1f676b58c30b4ae114b31a3b5826a2
Travel Devel <traveldevel@hotmail.com>
2017-09-24 20:13:11 +0300

    updated for new architecture with 4 different Mongo DB Services, one for each OData microservice in the tenant

--- index.js
+++ index.js
@@ -20,32 +20,43 @@ const authorizedUserPasswords = process.env.BASIC_AUTH_USER_PASSWORDS.split(',')
 
 // configs from env vars
 var appEnv = cfenv.getAppEnv();
-//console.log(appEnv.getServices());
+const services = appEnv.getServices();
+//console.log(services);
 
-var mongoServiceName = "iot_hub_mongo_" + landscapeName;
-var mongoService = appEnv.getService(mongoServiceName);
-var mongoCredentials = appEnv.getServiceCreds(mongoServiceName);
-var mongoUrl = mongoCredentials.uri;
-var mongoClient = require('mongodb').MongoClient;
+// get mongo url from service function
+var getMongoUrlForService = function(mongoServiceName) {
 
-console.log(mongoServiceName + " found in VCAP_SERVICES");
-console.log(mongoService.credentials);
+    var mongoService = services[mongoServiceName];
 
-var mongoDbName = '';
-var mongoUrl = '';
+    var mongoCredentials = {};
+    
+    var mongoUrl = '';
+    var mongoDbName = '';
 
-if(mongoService !== undefined){
+    if(mongoService !== undefined){
+        mongoCredentials = services[mongoServiceName].credentials;
 
-    mongoUrl = mongoService.credentials.uri + "?ssl=false";
+        mongoUrl = mongoService.credentials.uri;
+        
+        var mongodbUri = require('mongodb-uri');
+        var uriObject = mongodbUri.parse(mongoUrl);
+        mongoDbName = uriObject.database;
+    }
 
-    var mongodbUri = require('mongodb-uri');
-    var uriObject = mongodbUri.parse(mongoUrl);
-    mongoDbName = uriObject.database;
+    console.log("'" + mongoServiceName + "' found in VCAP_SERVICES ! ");
+    console.log("Url for mongodb : '" + mongoUrl + "'");
+    console.log("DB for mongodb : '" + mongoDbName + "'");
+
+    return { "url" : mongoUrl, "db" : mongoDbName};
 }
 
-console.log("Mongo url : ", mongoUrl);
-console.log("Mongo db : ", mongoDbName);
+// get mongoDb Url fo metadata service
+const mongoServiceBaseName = "iot_hub_mongo_" + landscapeName + "_" + tenantName;
+var mongoConnData = getMongoUrlForService(mongoServiceBaseName + "_metadata");
+const mongoUrl = mongoConnData.url; 
+const mongoDbName = mongoConnData.db;
 
+// odata service model
 var model = {
     namespace: mongoDbName,
     entityTypes: {
@@ -84,71 +95,16 @@ var model = {
             "last_contact": { "type": "Edm.DateTime"},
             "mandatory_schema_id": { "type": "Edm.String", key: true}, 
             "validate_schema": { "type": "Edm.Boolean"}
-        },
-        'location':{
-            "_id": { "type": "Edm.String", key: true},
-            "project_id": { "type": "Edm.String"},
-            "group_id": { "type": "Edm.String"}, 
-            "device_id": { "type": "Edm.String"},
-            "latitude": { "type": "Edm.Decimal"},
-            "longitude": { "type": "Edm.Decimal"},
-            "accuracy": { "type": "Edm.Integer"},
-            "speed": { "type": "Edm.Integer"},
-            "recorded_time": { "type": "Edm.DateTime"},
-            "created_at": { "type": "Edm.DateTime"}
-        },
-        'raw_data':{
-            "_id": { "type": "Edm.String", key: true},
-            "project_id": { "type": "Edm.String"},
-            "group_id": { "type": "Edm.String"}, 
-            "device_id": { "type": "Edm.String"},
-            "recorded_time": { "type": "Edm.DateTime"},
-            "created_at": { "type": "Edm.DateTime"},
-            "values" : { "type": "Collection(Edm.String)"}
-        },  
-        'event':{
-            "_id": { "type": "Edm.String", key: true},
-            "project_id": { "type": "Edm.String"},
-            "group_id": { "type": "Edm.String"}, 
-            "device_id": { "type": "Edm.String"},
-            "type": { "type": "Edm.String"},
-            "text": { "type": "Edm.String"},
-            "dismissed": { "type": "Edm.String"},
-            "user_id": { "type": "Edm.String"}         
-        },  
-        'event_rule':{
-            "_id": { "type": "Edm.String", key: true},
-            "project_id": { "type": "Edm.String"},
-            "group_id": { "type": "Edm.String"}, 
-            "device_id": { "type": "Edm.String"},
-            "rule_name": { "type": "Edm.String"},
-            "operator": { "type": "Edm.String"},
-            "operator_reference": { "type": "Edm.String"}      
-        },         
-        'command':{
-            "_id": { "type": "Edm.String", key: true},
-            "project_id": { "type": "Edm.String"},
-            "group_id": { "type": "Edm.String"}, 
-            "device_id": { "type": "Edm.String"},
-            "type": { "type": "Edm.String"},
-            "command": { "type": "Edm.String"},
-            "created_at": { "type": "Edm.DateTime"},
-            "confirmed_at": { "type": "Edm.DateTime"}      
-        }   
+        }
     },   
     entitySets: {}
 };
 
-model.entitySets[tenantName + "_user"] = { entityType: mongoDbName + ".user" };
-model.entitySets[tenantName + "_project"] = { entityType: mongoDbName + ".project" };
-model.entitySets[tenantName + "_device_group"] = { entityType: mongoDbName + ".device_group" };
-model.entitySets[tenantName + "_device_schema"] = { entityType: mongoDbName + ".device_schema" };
-model.entitySets[tenantName + "_device"] = { entityType: mongoDbName + ".device" };
-model.entitySets[tenantName + "_location"] = { entityType: mongoDbName + ".location" };
-model.entitySets[tenantName + "_raw_data"] = { entityType: mongoDbName + ".raw_data" };
-model.entitySets[tenantName + "_event"] = { entityType: mongoDbName + ".event" };
-model.entitySets[tenantName + "_event_rule"] = { entityType: mongoDbName + ".event_rule" };
-model.entitySets[tenantName + "_command"] = { entityType: mongoDbName + ".command" };
+model.entitySets["user"] = { entityType: mongoDbName + ".user" };
+model.entitySets["project"] = { entityType: mongoDbName + ".project" };
+model.entitySets["device_group"] = { entityType: mongoDbName + ".device_group" };
+model.entitySets["device_schema"] = { entityType: mongoDbName + ".device_schema" };
+model.entitySets["device"] = { entityType: mongoDbName + ".device" };
 
 // Instantiates ODataServer and assigns to odataserver variable.
 var odataServer = ODataServer().model(model);
@@ -160,6 +116,8 @@ odataServer.error(function(req, res, error, next){
 })
 
 // Connection to database in MongoDB
+var mongoClient = require('mongodb').MongoClient;
+
 MongoClient.connect(mongoUrl, function(err, db) {
     
     if(err){
@@ -200,5 +158,5 @@ app.use("/", auth, function (req, res) {
 
 // The app listens on port 8080 (or other from env) and prints the endpoint URI in console window.
 var server = app.listen(port, function () {
-    console.log('OData service listening on ' + appEnv.url + ':' + process.env.PORT);
+    console.log('Metadata OData service listening on ' + appEnv.url + ':' + process.env.PORT);
 });
